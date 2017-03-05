85855cab8877dd0352c67a060f22dd09d6060621
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-03-05 19:49:21 +0100

    Fixed the uses of the Time class in the TestShow.java. Also added the "Name" field to the parameters when a show is created.

--- src/TestShow.java
+++ src/TestShow.java
@@ -1,8 +1,8 @@
-import java.sql.Time;
 import java.util.ArrayList;
 
 /**
  * Created by dionb on 13-2-2017.
+ * Edited by Arthur van Strien on 05-03-2017.
  */
 public class TestShow {
 
@@ -12,15 +12,18 @@ public class TestShow {
         ArrayList<Artist> artists = new ArrayList<>();
         artists.add(new Artist("artist", "genre"));
         artists.add(new Artist("artist2", "genre2"));
-        Show s1 = new Show(new Time(12, 0, 0), new Time(16,0, 0), new Stage("stage", 50), artists,0.5);
+        Time beginTime = new Time(12, 0);
+        Time endTime = new Time(16,0);
+        Stage stage = new Stage("Main Stage", 50);
+        Show s1 = new Show("Jimi Hendrix Experience", beginTime, endTime, stage, artists, 5);
 
         if(!s1.toString().equals("Begint om: 12:00:00 eindigd om: 16:00:00 Stage{name='stage'} [Artiest: artist Genre: genre, Artiest: artist2 Genre: genre2]")) {
-            if(!s1.getBeginTime().equals(new Time(12, 0, 0))) {
+            if(!s1.getBeginTime().equals(new Time(12, 0))) {
                 System.out.println("There is a problem with the begin time in the getBeginTime method from the Show class .");
                 amountOfErrors++;
             }
 
-            if(!s1.getEndTime().equals(new Time(16, 0, 0))) {
+            if(!s1.getEndTime().equals(new Time(16, 0))) {
                 System.out.println("There is a problem with the getEndTime method from the Show class.");
                 amountOfErrors++;
             }
