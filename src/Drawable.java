aaa4f8ec904cccf836b9c0e2bc62ad8f7b28857b
ROBIN-PC\robin <rm.hobbel@telfort.nl>
2017-03-12 17:29:14 +0100

    added a drwaable interface and a visitor class. Added Images. First version -Not functional, Not tested-

---
+++ src/Drawable.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+/**
+ * Created by robin on 12-3-2017.
+ */
+public interface Drawable {
+
+    void draw(Graphics2D g2d);
+    void update(ArrayList<Drawable> drawings);
+    Point2D getPosition();
+
+}
