ef770f1eca70623ca3d9da62aec38082b9609e59
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-03-13 23:03:16 +0100

    added TestAddArtistsGUI

---
+++ src/TestAddArtistsGUI.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Arthur on 13-3-2017.
+ */
+public class TestAddArtistsGUI {
+    public static int runTest() {
+        int errors = 0;
+
+        //Create a new AddArtistGUI with the given values.
+        //AddArtistsGUI addArtistGUI = new AddArtistsGUI();
+
+
+
+        return errors;
+    }
+}
